311eb5b31e75f528597fffeaba5b5e10195943bf
Magomed <makhas18@gmail.com>
2022-01-19 12:39:15 +0300

    Добавлены правки по данным советам

--- src/tutorial-2/components/AuthForm.tsx
+++ src/tutorial-2/components/AuthForm.tsx
@@ -6,29 +6,30 @@ export const Form: React.FC = () => {
   let password: string = "";
 
   function formChange(event: React.ChangeEvent<HTMLInputElement>) {
-    if (event.target.type === "text") {
+    if (event.target.name === "email") {
       email = event.target.value
-    } else if (event.target.type === "password") {
+    } else if (event.target.name === "password") {
       password = event.target.value;
     }
   }
   
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    if ( email.trim() === "" || password.trim() === "") {
+    if ( !email.trim() || !password.trim()) {
       alert('Enter your auth data!')
     } else {
       console.log( {email, password} );
-      [email, password] = ['', '']
-      event.target.reset()
+      email = '';
+      password = '';
+      event.target.reset();
     }
   }
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit} className="form">
-        <input onChange={formChange} className="form_email" type="text" placeholder="E-mail"/>
-        <input onChange={formChange} className="form_password" type="password" placeholder="Пароль"/>
+        <input onChange={formChange} className="form_email" type="text" name="email" placeholder="E-mail"/>
+        <input onChange={formChange} className="form_password" type="password" name="password" placeholder="Пароль"/>
         <button className="form_btn" type="submit"><b>Войти</b></button>
       </form>
     </div>
