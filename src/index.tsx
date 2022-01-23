54484502259da0e6c0fae14006163609c9678f7a
Magomed <makhas18@gmail.com>
2022-01-23 18:38:36 +0300

    Урок №7. Вывод полной записи по отдельному пути.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import ReactDOM from "react-dom";
 //import App from './tutorial-2/App';
 //import App from "./tutorial-3/App";
 //import App from "./tutorial-5/App";
-import App from "./tutorial-6/App";
+//import App from "./tutorial-6/App";
+import App from "./tutorial-7/App";
+import styles from "./tutorial-7/App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
